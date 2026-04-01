73b46aea7daf8cffcc38ed8d55b95215ae596301
Buddy Stone <buddy.cgn@gmail.com>
2026-04-01 20:59:41 +0200

    added location images + fallback

--- src/features/adventure/components/AdventureLocations.tsx
+++ src/features/adventure/components/AdventureLocations.tsx
@@ -17,13 +17,29 @@ function AdventureLocations() {
         label: `${item.roll}: ${item.move ? `(M${item.move}) ` : ''}${item.name}`,
         children: (
             <>
-                <p>
-                    {item.terrain && <em className="mr-1">({item.terrain})</em>}
-                    {item.description}
-                </p>
+                <div className="flex flex-row items-top">
+                    <div className="mt-3 mr-5">
+                        <img
+                            src={`/images/locations/${tag}-${index + 1}.png`}
+                            className="w-16"
+                            alt={item.name}
+                            onError={(e) => {
+                                const img = e.currentTarget
+                                img.onerror = null
+                                img.src = `/images/adventures/${tag}.png`
+                            }}
+                        />
+                    </div>
+                    <p>
+                        {item.terrain && (
+                            <em className="mr-1">({item.terrain})</em>
+                        )}
+                        {item.description}
+                    </p>
+                </div>
 
                 <p>
-                    <strong>Encounter ({item.encounter}):</strong>
+                    <strong>Encounter:</strong> <em>({item.encounter})</em>
                 </p>
 
                 {item.rolls && (
